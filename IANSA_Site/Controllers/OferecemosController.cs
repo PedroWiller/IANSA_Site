cdcfa0ff78189da00a7a8c9c320a88171be39c19
Lander Malta <lander.malta@hotmail.com>
2017-06-29 17:41:35 -0300

    Commit inicial

---
+++ IANSA_Site/Controllers/OferecemosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IANSA_Site.Controllers
+{
+    public class OferecemosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
